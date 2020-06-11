682e68fb4890284b275b8ade6a0c54dec63ede48
WrenScott <59579965+WrenScott@users.noreply.github.com>
2020-06-10 21:34:35 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -41,13 +41,13 @@ const Columbia_Slough_Watershed = L.esri.featureLayer({
     url: cswURL
 }).addTo(map);
 
-// Create pane for ORCA_CSW
-map.createPane('orca');
+// Create pane for water features & ORCA_CSW
+map.createPane('water');
 
 // Feature layer for Outdoor Recreation and Conservation Areas (ORCA) that are within the CSW 
 const ORCA_CSW = L.esri.featureLayer({
     url: orcaURL,
-    pane: 'orca'
+    pane: 'water'
 }).addTo(map);
 
 // Popup for ORCA 
